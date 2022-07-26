c7697e2b4d7ff5f5a381a95d3e24abdae71ec871
Chris Chambers <cchambers2@triumf.ca>
2022-07-26 14:22:56 -0700

    dataframe groupby method works

--- sakshiPy.py
+++ sakshiPy.py
@@ -8,28 +8,24 @@ os.listdir()
 
 data = pd.read_csv('run10109.csv',sep = ',')
 
-step_val = 0.0
-stop = False
-arr, data_arr = [], []
-desc_vals=[]
-for i in range(len(data['event'])):
-    desc = float(data['step_value_epics'][i][1:-1])
-    if  desc != step_val:
-        if stop == True:
+current_step = 0.0
+for i in range(len(data['step_value_epics'])):
+    if float(data['step_value_epics'][i][1:-1]) != current_step:
+        if float(data['step_value_epics'][i][1:-1]) < current_step:
+            data2 = data[:i]
             break
-        desc_vals.append(desc)
-        data_arr.append(arr)
-        arr = []
-        arr.append([data['tof'][i],data['dipole_freq'][i]])
-        step_val = desc
-        if desc == 2100.0:
-            stop = True
-    if desc == step_val:
-        arr.append([data['tof'][i],data['dipole_freq'][i]])
-data_arr = data_arr[1:]
-print(np.shape(data_arr[0]))
+        current_step = float(data['step_value_epics'][i][1:-1])
+        
+data_group = data2.groupby('step_value_epics')
 
-plt.figure('step_value_epics_'+str(desc_vals[0]))
-plt.plot(np.array(data_arr[0])[:,1], np.array(data_arr[0])[:,0], 'bo')
-plt.ylim(90,150)
+steps = list(data_group.groups.keys())
+
+print(data_group)
+
+plt.figure("Sakshi makes a plot")
+plt.clf()
+for i in steps:
+    plt.plot(data_group.get_group(i).groupby('dipole_freq')['tof'].mean(), '-o')
 plt.show()
+
+plt.savefig('sakshiPlot.jpg')
